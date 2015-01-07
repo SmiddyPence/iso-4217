581f5a91ec2a2cd41fb8cc6d0a30186f6760dcf6
Nicholas Smith <nicholas.p.smith@live.co.uk>
2015-01-07 09:35:31 +0000

    initial commit

---
+++ src/main/java/com/nps/iso/web/CurrencyController.java
@@ -0,0 +1,21 @@
+package com.nps.iso.web;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class CurrencyController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(CurrencyController.class, args);
+    }
+}
